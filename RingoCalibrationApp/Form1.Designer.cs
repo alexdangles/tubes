f145e94b03ad1ca90b0d46b87a8f6fa3b4d7f832
alexdangles <57923646+alexdangles@users.noreply.github.com>
2019-11-18 16:22:38 -0800

    Add files via upload

---
+++ RingoCalibrationApp/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace RingoCalibrationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Tube1 = new Devices.ctlTube();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pass = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DPP1 = new Devices.Controls.ctlDPP50();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.FW1 = new Devices.ctlFilterWheel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(3, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1044, 672);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Tube1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.btnVerify);
+            this.tabPage1.Controls.Add(this.btnStart);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1036, 646);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Tube1
+            // 
+            this.Tube1.AutoSize = true;
+            this.Tube1.BackColor = System.Drawing.SystemColors.Control;
+            this.Tube1.Location = new System.Drawing.Point(6, 7);
+            this.Tube1.Name = "Tube1";
+            this.Tube1.Size = new System.Drawing.Size(240, 309);
+            this.Tube1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Pass});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 322);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(481, 317);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "kV";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "kV Set";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "kV Mon";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "kV Edge";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Pass
+            // 
+            this.Pass.HeaderText = "Pass";
+            this.Pass.Name = "Pass";
+            this.Pass.ReadOnly = true;
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.BackColor = System.Drawing.Color.LightBlue;
+            this.btnVerify.Location = new System.Drawing.Point(493, 492);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(103, 42);
+            this.btnVerify.TabIndex = 2;
+            this.btnVerify.Text = "Verify";
+            this.btnVerify.UseVisualStyleBackColor = false;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LightBlue;
+            this.btnStart.Location = new System.Drawing.Point(493, 444);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(103, 42);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(493, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(537, 431);
+            this.panel1.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.DPP1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1036, 646);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "DPP";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // DPP1
+            // 
+            this.DPP1.acquiring = false;
+            this.DPP1.bremstrahlungEV = ((uint)(0u));
+            this.DPP1.Location = new System.Drawing.Point(3, 6);
+            this.DPP1.Name = "DPP1";
+            this.DPP1.roi = new int[] {
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0};
+            this.DPP1.setTime = 0D;
+            this.DPP1.Size = new System.Drawing.Size(699, 638);
+            this.DPP1.TabIndex = 2;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.FW1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1036, 646);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Other";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // FW1
+            // 
+            this.FW1.AutoSize = true;
+            this.FW1.Enabled = false;
+            this.FW1.Location = new System.Drawing.Point(6, 16);
+            this.FW1.Name = "FW1";
+            this.FW1.Size = new System.Drawing.Size(153, 153);
+            this.FW1.TabIndex = 1;
+            this.FW1.Tag = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1059, 673);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Devices.ctlFilterWheel FW1;
+        private Devices.Controls.ctlDPP50 DPP1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.Button btnStart;
+        private Devices.ctlTube Tube1;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Pass;
+    }
+}
+
---
+++ RingoCalibrationApp/Form1.cs
@@ -0,0 +1,264 @@
+﻿using Helper;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RingoCalibrationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Controls.Add(DPP1.spmGraph);
+            DPP1.numkeV.Enabled = false;
+
+            DPP1.AutoConnect(Text);
+            Tube1.AutoConnect(Text);
+            FW1.AutoConnect(Text);
+
+            DPP1.LoadSettings(
+                $@"{ Tube1.configFolder }PS564 RH3 XPIN_BT\MX-02r00us.dxp",
+                $@"{ Tube1.configFolder }start upcalibration\Ringo calibration.txt"
+                );
+            Text = Tube1.testbox;
+            Tube1.numSetkV.ValueChanged += NumSetkV_ValueChanged;
+        }
+
+        private void NumSetkV_ValueChanged(object sender, EventArgs e)
+        {
+            DPP1.numkeV.Value = Tube1.numSetkV.Value;
+        }
+
+        /// <summary>
+        /// Generate an auto-incrementing test ID in tubes database.
+        /// </summary>
+        /// <param name="serial">Tube serial number.</param>
+        /// <param name="testbox">Testbox name.</param>
+        /// <returns></returns>
+        public static int GenerateTestID(int serial, string testbox)
+        {
+            DBHelper.Insert("TSCBTest", serial, DateTime.Now, testbox);
+            return Convert.ToInt32(DBHelper.Query("select MAX(TEST_ID_PK) from TSCBTest").Rows[0][0]);
+        }
+
+        private void DoTest(bool justVerify = false)
+        {
+            int testID = GenerateTestID(Tube1.tub.serialNum, Tube1.testboxShort);
+            double maxkV = Tube1.tub.maxkV;
+
+            btnStart.Enabled = btnVerify.Enabled = false;
+            Tube1.tub.status = Devices.Tube.Status.Testing;
+            Tube1.tub.maxkV = 55;
+
+            int n = 0;
+
+            double[] kV;
+            double[] kVSet;
+            double[] kVMon;
+            double[] kVEdge;
+            bool[] pass;
+
+            List<double> verifyKV = new List<double>();
+            if (Tube1.tub.maxkV == 35) verifyKV.Add(35);
+            else
+            {
+                verifyKV.Add(8);
+                verifyKV.Add(13);
+                verifyKV.Add(15);
+                verifyKV.Add(25);
+                verifyKV.Add(35);
+                verifyKV.Add(40);
+                if (Tube1.tub.maxkV > 40) verifyKV.Add(50);
+            }
+
+            if (justVerify) n = 1;
+
+            for (int j = n; j < 2; j++)
+            {
+                if (n == 0)
+                {
+                    kV = new double[48];
+                    kVSet = new double[48];
+                    kVMon = new double[48];
+                    kVEdge = new double[48];
+                    pass = new bool[48];
+                    dataGridView1.Columns["Pass"].Visible = false;
+
+                    Tube1.tub.DeleteCalibration();
+                }
+                else
+                {
+                    kV = new double[verifyKV.Count];
+                    kVSet = new double[verifyKV.Count];
+                    kVMon = new double[verifyKV.Count];
+                    kVEdge = new double[verifyKV.Count];
+                    pass = new bool[verifyKV.Count];
+                    dataGridView1.Columns["Pass"].Visible = true;
+                }
+
+                Tube1.tub.setuA = Tube1.tub.maxuA;
+                Tube1.tub.on = true;
+                dataGridView1.Rows.Clear();
+
+                for (int i = 0; i < kV.Length; i++)
+                {
+                    if (n == 0)
+                        kV[i] = i + 5;
+                    else
+                        kV[i] = verifyKV[i];
+
+                    if (kV[i] <= 11)
+                    {
+                        FW1.ChangeFilter(1);
+                    }
+                    else if (kV[i] >= 12 && kV[i] <= 16)
+                    {
+                        FW1.ChangeFilter(2);
+                    }
+                    else if (kV[i] >= 17 && kV[i] <= 24)
+                    {
+                        FW1.ChangeFilter(3);
+                    }
+                    else if (kV[i] >= 25)
+                    {
+                        FW1.ChangeFilter(3);
+                    }
+
+                    Tube1.tub.setkV = kV[i];
+
+                    Tools.WaitSync(1000);
+
+                    DPP1.GetSpectrum();
+
+                    if (DPP1.stopper) break;
+
+                    kVSet[i] = Tube1.tub.setkV;
+                    kVMon[i] = Tube1.tub.monkV;
+                    kVEdge[i] = DPP1.keVEdge;
+
+                    if (n == 0) dataGridView1.Rows.Add(kV[i], kVSet[i], kVMon[i], kVEdge[i]);
+                    else
+                    {
+                        pass[i] = (kVEdge[i] < kV[i] + 0.2) && (kVEdge[i] > kV[i] - 0.2);
+                        dataGridView1.Rows.Add(kV[i], kVSet[i], kVMon[i], kVEdge[i], pass[i]);
+                    }
+                }
+
+                Tube1.tub.on = false;
+
+                if (DPP1.stopper) break;
+
+                if (n == 0)
+                {
+                    Tube1.tub.Calibrate(kVEdge);
+                    n++;
+
+                    Tools.WaitSync(2000);
+
+                    for (int i = 0; i < kV.Length; i++)
+                    {
+                        DBHelper.Insert("TSCBPreCalData",
+                            testID,
+                            Tube1.tub.serialNum,
+                            kV[i],
+                            kVSet[i],
+                            kVMon[i],
+                            kVEdge[i]
+                            );
+                    }
+
+                    for (int i = 0; i < Tube1.tub.keVFit.Length; i++)
+                    {
+                        DBHelper.Insert("TSCBSourceCalibration",
+                            testID,
+                            Tube1.tub.serialNum,
+                            Tube1.tub.keVFit[i],
+                            Tube1.tub.keVFitByte[i],
+                            Tube1.tub.page1keVAddresses[i],
+                            Tube1.tub.keVSetFitByte[i],
+                            Tube1.tub.page1SetAddresses[i],
+                            Tube1.tub.keVMonFitByte[i],
+                            Tube1.tub.page1MonAddresses[i]
+                            );
+                    }
+                }
+                else if (n == 1)
+                {
+                    for (int i = 0; i < kV.Length; i++)
+                    {
+                        DBHelper.Insert("TSCBSourceCalValidation",
+                            testID,
+                            Tube1.tub.serialNum,
+                            kV[i],
+                            kVSet[i],
+                            kVMon[i],
+                            kVEdge[i],
+                            0,
+                            0,
+                            pass[i]
+                            );
+                    }
+
+                    Tube1.tub.status = Devices.Tube.Status.Done;
+
+                    string result = "";
+                    for (int i = 0; i < kV.Length; i++)
+                    {
+                        result += $"{ kV[i] }  { kVEdge[i] }  { pass[i] }\r\n";
+                    }
+                    result = result.Replace("True", "Yes");
+                    result = result.Replace("False", "No");
+                    MessageBox.Show(
+                        "kV   keVEdge   Pass?\r\n" +
+                        result, "Test Results");
+                }
+            }
+
+            Tube1.tub.maxkV = maxkV;
+            Tube1.tub.status = Devices.Tube.Status.Idle;
+
+            DPP1.stopper = false;
+            btnStart.Enabled = btnVerify.Enabled = true;
+            btnStart.BackColor = btnVerify.BackColor = Color.LightBlue;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (Tube1.tub.status != Devices.Tube.Status.Error && Tube1.tub.controlType == Devices.Tube.ControlType.I2C)
+            {
+                btnStart.BackColor = Color.Aqua;
+                DoTest();
+            }
+        }
+
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            if (Tube1.tub.status != Devices.Tube.Status.Error && Tube1.tub.controlType == Devices.Tube.ControlType.I2C)
+            {
+                btnVerify.BackColor = Color.Aqua;
+                DoTest(true);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Tube1.tub.status == Devices.Tube.Status.Testing)
+            {
+                var result = MessageBox.Show("End?", "End Testing?", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    DPP1.stopper = true;
+                }
+                e.Cancel = true;
+            }
+            else
+                MessageBox.Show("Open the interlock, then press OK.");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+        }
+    }
+}
