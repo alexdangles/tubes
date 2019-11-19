794f8b94666352979bee99f769f86447495cf7e3
alexdangles <57923646+alexdangles@users.noreply.github.com>
2019-11-18 16:24:58 -0800

    Add files via upload

---
+++ ThorLabsFWApp/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ThorLabsFWApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctlFilterWheel1 = new Devices.Controls.ctlFilterWheel();
+            this.SuspendLayout();
+            // 
+            // ctlFilterWheel1
+            // 
+            this.ctlFilterWheel1.AutoSize = true;
+            this.ctlFilterWheel1.Enabled = false;
+            this.ctlFilterWheel1.Location = new System.Drawing.Point(0, 1);
+            this.ctlFilterWheel1.Name = "ctlFilterWheel1";
+            this.ctlFilterWheel1.Size = new System.Drawing.Size(153, 153);
+            this.ctlFilterWheel1.TabIndex = 0;
+            this.ctlFilterWheel1.Tag = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(154, 150);
+            this.Controls.Add(this.ctlFilterWheel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "FW102c";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Devices.Controls.ctlFilterWheel ctlFilterWheel1;
+    }
+}
+
---
+++ ThorLabsFWApp/Form1.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace ThorLabsFWApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ctlFilterWheel1.AutoConnect();
+        }
+    }
+}
