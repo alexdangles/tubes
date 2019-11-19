39949e077453354d43026df5d681d5e50c5983c1
alexdangles <57923646+alexdangles@users.noreply.github.com>
2019-11-18 16:20:37 -0800

    Add files via upload

---
+++ NIcDAQApp/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace NIcDAQApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ctlGraph1 = new Helper.ctlGraph();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.ledArray1 = new NationalInstruments.UI.WindowsForms.LedArray();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ledArray1.ItemTemplate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(174, 190);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Digital Out";
+            this.groupBox1.Visible = false;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.HorizontalScrollbar = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "DO1"});
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 18);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(165, 152);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.DigitalWrite);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.ctlGraph1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(385, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(846, 491);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Analog In";
+            this.groupBox2.Visible = false;
+            // 
+            // ctlGraph1
+            // 
+            this.ctlGraph1.BackColor = System.Drawing.Color.White;
+            this.ctlGraph1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ctlGraph1.Enabled = false;
+            this.ctlGraph1.ForeColor = System.Drawing.Color.Black;
+            this.ctlGraph1.LegendVisible = true;
+            this.ctlGraph1.Location = new System.Drawing.Point(3, 17);
+            this.ctlGraph1.mode = Helper.ctlGraph.Mode.Scope;
+            this.ctlGraph1.Name = "ctlGraph1";
+            this.ctlGraph1.Size = new System.Drawing.Size(840, 471);
+            this.ctlGraph1.TabIndex = 0;
+            this.ctlGraph1.Title = "";
+            this.ctlGraph1.xAxis = "Time";
+            this.ctlGraph1.yAxis = "Volts";
+            this.ctlGraph1.yAxisSecondary = "";
+            this.ctlGraph1.Load += new System.EventHandler(this.ctlGraph1_Load);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.numericUpDown1);
+            this.groupBox3.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox3.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(192, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(187, 477);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Analog Out";
+            this.groupBox3.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(10, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AO1";
+            this.label1.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.AutoSize = true;
+            this.numericUpDown1.BackColor = System.Drawing.Color.Gainsboro;
+            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.Black;
+            this.numericUpDown1.Location = new System.Drawing.Point(10, 38);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.numericUpDown1.Visible = false;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.AnalogWrite);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 17);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(181, 457);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(198, 147);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(5, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Selected Tasks";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.LoadTasks);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(5, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 32);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Setup Tasks in NI MAX";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(5, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 32);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Refresh List";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1234, 497);
+            this.tableLayoutPanel1.TabIndex = 7;
+            this.tableLayoutPanel1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox4);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(183, 474);
+            this.panel1.TabIndex = 3;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.AutoSize = true;
+            this.groupBox4.Controls.Add(this.ledArray1);
+            this.groupBox4.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(4, 190);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(174, 278);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Digital In";
+            this.groupBox4.Visible = false;
+            // 
+            // ledArray1
+            // 
+            this.ledArray1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ledArray1.ItemDistance = 0;
+            // 
+            // 
+            // 
+            this.ledArray1.ItemTemplate.LedStyle = NationalInstruments.UI.LedStyle.Round3D;
+            this.ledArray1.ItemTemplate.Location = new System.Drawing.Point(0, 0);
+            this.ledArray1.ItemTemplate.Name = "";
+            this.ledArray1.ItemTemplate.Size = new System.Drawing.Size(32, 32);
+            this.ledArray1.ItemTemplate.TabIndex = 0;
+            this.ledArray1.ItemTemplate.TabStop = false;
+            this.ledArray1.Location = new System.Drawing.Point(3, 17);
+            this.ledArray1.Name = "ledArray1";
+            this.ledArray1.ScaleMode = NationalInstruments.UI.ControlArrayScaleMode.CreateFixedMode(0);
+            this.ledArray1.Size = new System.Drawing.Size(168, 258);
+            this.ledArray1.TabIndex = 0;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1234, 497);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "cDAQ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmDAQ_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ledArray1.ItemTemplate)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private Helper.ctlGraph ctlGraph1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private NationalInstruments.UI.WindowsForms.LedArray ledArray1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ NIcDAQApp/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Devices;
+using Helper;
+using System.IO;
+using System.Diagnostics;
+using NationalInstruments.UI;
+
+namespace NIcDAQApp
+{
+    /// <summary>
+    /// National Instruments cDAQ User Interface.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        NIcDAQ daq;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.DataSource = NIcDAQ.taskList;
+        }
+
+        private void frmDAQ_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            daq?.Disconnect();
+        }
+
+        private void DigitalWrite(object sender, EventArgs e)
+        {
+            List<bool> relay = new List<bool>();
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                relay.Add(checkedListBox1.GetItemChecked(i));
+            }
+            daq.DigitalWrite(relay.ToArray());
+        }
+
+        private void AnalogWrite(object sender, EventArgs e)
+        {
+            List<double> volts = new List<double>();
+            foreach (var n in flowLayoutPanel1.Controls.OfType<NumericUpDown>())
+                volts.Add((double)n.Value);
+
+            daq.AnalogWrite(volts.ToArray());
+        }
+
+        private void LoadTasks(object sender, EventArgs e)
+        {
+            try
+            {
+                string tasks = "";
+                foreach (string s in listBox1.SelectedItems)
+                {
+                    if (tasks.Length > 0) tasks += "," + s;
+                    else tasks = s;
+                }
+
+                daq = new NIcDAQ(tasks, false);
+                Thread.Sleep(500);
+
+                if (daq.DOChannels != null)
+                {
+                    groupBox1.Show();
+                    checkedListBox1.Items.Clear();
+                    for (int i = 0; i < daq.DOChannels.Length; i++)
+                    {
+                        checkedListBox1.Items.Add(daq.DOChannels[i]);
+                    }
+                }
+
+                if (daq.AIChannels != null)
+                {
+                    groupBox2.Show();
+                    List<string> names = new List<string>();
+                    for (int i = 0; i < daq.AIChannels.Length; i++)
+                    {
+                        names.Add(daq.AIChannels[i]);
+                    }
+                    ctlGraph1.AddPlots(names.ToArray());
+                    timer1.Start();
+                }
+
+                if (daq.AOChannels != null)
+                {
+                    groupBox3.Show();
+                    for (int i = 0; i < daq.AOChannels.Length; i++)
+                    {
+                        Label l = MyControls.Clone(label1);
+                        l.Text = daq.AOChannels[i];
+                        NumericUpDown n = MyControls.Clone(numericUpDown1);
+                        l.Visible = n.Visible = true;
+                        n.ValueChanged += AnalogWrite;
+                        flowLayoutPanel1.Controls.Add(l);
+                        flowLayoutPanel1.Controls.Add(n);
+                    }
+                }
+
+                if (daq.DIChannels != null)
+                {
+                    groupBox4.Show();
+                    ledArray1.ScaleMode = ControlArrayScaleMode.CreateFixedMode(daq.digitalInData.Length);
+                    timer2.Start();
+                }
+
+                listBox1.Hide();
+                button1.Hide();
+                button2.Hide();
+                button3.Hide();
+                tableLayoutPanel1.Show();
+
+                if (groupBox2.Visible)
+                {
+                    AutoSize = false;
+                    AutoSizeMode = AutoSizeMode.GrowOnly;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("DAQ Assistant Error. Needs reset.");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (string s in daq.AIChannels)
+                ctlGraph1.Plot(s, 0, daq.GetData(s));
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = listBox1.SelectedItems.Count > 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string progFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            string file = $"{ progFiles }\\National Instruments\\MAX\\NIMax.exe";
+            if (File.Exists(file))
+            {
+                Process.Start(file);
+            }
+            else MessageBox.Show("Cannot find Measurement and Automation Explorer installed on this PC.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.DataSource = NIcDAQ.taskList;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < daq.digitalInData.Length; i++)
+            {
+                ledArray1[i].Value = daq.digitalInData[i];
+            }
+        }
+
+        private void ctlGraph1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
